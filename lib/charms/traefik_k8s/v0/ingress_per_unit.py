1a9987820307ee841dba4d62203c7feaea8d2cf5
Pietro Pasotti <starfire.daemon@gmail.com>
2022-04-14 11:11:26 +0200

    bumped LIBPATCH

--- lib/charms/traefik_k8s/v0/ingress_per_unit.py
+++ lib/charms/traefik_k8s/v0/ingress_per_unit.py
@@ -73,7 +73,7 @@ LIBAPI = 0
 
 # Increment this PATCH version before using `charmcraft publish-lib` or reset
 # to 0 if you are raising the major API version
-LIBPATCH = 8
+LIBPATCH = 9
 
 log = logging.getLogger(__name__)
 
