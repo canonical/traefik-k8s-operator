c0a5ca705675c338bfd9e313bdd0a94e22f1f29c
Pietro Pasotti <starfire.daemon@gmail.com>
2022-05-31 16:56:24 +0200

    added ipa and ipu itests with prometheus and spring-music

---
+++ tests/integration/conftest.py
@@ -0,0 +1,157 @@
+# Copyright 2022 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+import asyncio
+from dataclasses import dataclass
+from pathlib import Path
+from subprocess import PIPE, Popen
+
+import pytest
+import yaml
+
+_JUJU_DATA_CACHE = {}
+_JUJU_KEYS = ("egress-subnets", "ingress-address", "private-address")
+
+
+@pytest.fixture(autouse=True, scope="session")
+def traefik_charm():
+    Popen(["charmcraft", "pack"]).wait()
+    charms = tuple(map(str, Path().glob("*.charm")))
+    assert len(charms) == 1, (
+        f"too many charms {charms}" if charms else f"no charm found at {Path().absolute()}"
+    )
+
+    charm = charms[0]
+    charm_path = Path(charm).absolute()
+
+    assert charm_path.exists()
+
+    yield charm_path
+
+    # Popen(['rm', str(charm_path)])
+
+
+def purge(data: dict):
+    for key in _JUJU_KEYS:
+        if key in data:
+            del data[key]
+
+
+async def grab_unit_info(unit_name: str) -> dict:
+    """Returns unit-info data structure.
+
+     for example:
+
+    traefik-k8s/0:
+      opened-ports: []
+      charm: local:focal/traefik-k8s-1
+      leader: true
+      relation-info:
+      - endpoint: ingress-per-unit
+        related-endpoint: ingress
+        application-data:
+          _supported_versions: '- v1'
+        related-units:
+          prometheus-k8s/0:
+            in-scope: true
+            data:
+              egress-subnets: 10.152.183.150/32
+              ingress-address: 10.152.183.150
+              private-address: 10.152.183.150
+      provider-id: traefik-k8s-0
+      address: 10.1.232.144
+    """
+    if cached_data := _JUJU_DATA_CACHE.get(unit_name):
+        return cached_data
+
+    proc = Popen(f"juju show-unit {unit_name}".split(" "), stdout=PIPE)
+    raw_data = proc.stdout.read().decode("utf-8").strip()
+    if not raw_data:
+        raise ValueError(
+            f"no unit info could be grabbed for {unit_name}; "
+            f"are you sure it's a valid unit name?"
+        )
+
+    data = yaml.safe_load(raw_data)
+    _JUJU_DATA_CACHE[unit_name] = data
+    return data
+
+
+def get_relation_by_endpoint(relations, endpoint, remote_obj):
+    relations = [
+        r for r in relations if r["endpoint"] == endpoint and remote_obj in r["related-units"]
+    ]
+    if not relations:
+        raise ValueError(f"no relations found with endpoint==" f"{endpoint}")
+    if len(relations) > 1:
+        raise ValueError("multiple relations found with endpoint==" f"{endpoint}")
+    return relations[0]
+
+
+@dataclass
+class UnitRelationData:
+    unit_name: str
+    endpoint: str
+    leader: bool
+    application_data: dict
+    unit_data: dict
+
+
+async def get_content(
+    obj: str, other_obj, include_default_juju_keys: bool = False
+) -> UnitRelationData:
+    """Get the content of the databag of `obj`, relative to `other_obj`."""
+    endpoint = None
+    other_unit_name = other_obj.split(":")[0] if ":" in other_obj else other_obj
+    if ":" in obj:
+        unit_name, endpoint = obj.split(":")
+    else:
+        unit_name = obj
+    data = (await grab_unit_info(unit_name))[unit_name]
+    is_leader = data["leader"]
+
+    relation_infos = data.get("relation-info")
+    if not relation_infos:
+        raise RuntimeError(f"{unit_name} has no relations")
+
+    if not endpoint:
+        relation_data_raw = relation_infos[0]
+        endpoint = relation_data_raw["endpoint"]
+    else:
+        relation_data_raw = get_relation_by_endpoint(relation_infos, endpoint, other_unit_name)
+
+    related_units_data_raw = relation_data_raw["related-units"]
+
+    other_unit_name = next(iter(related_units_data_raw.keys()))
+    other_unit_info = await grab_unit_info(other_unit_name)
+    other_unit_relation_infos = other_unit_info[other_unit_name]["relation-info"]
+    remote_data_raw = get_relation_by_endpoint(
+        other_unit_relation_infos, relation_data_raw["related-endpoint"], unit_name
+    )
+    this_unit_data = remote_data_raw["related-units"][unit_name]["data"]
+    this_app_data = remote_data_raw["application-data"]
+
+    if not include_default_juju_keys:
+        purge(this_unit_data)
+
+    return UnitRelationData(unit_name, endpoint, is_leader, this_app_data, this_unit_data)
+
+
+@dataclass
+class RelationData:
+    provider: UnitRelationData
+    requirer: UnitRelationData
+
+
+async def get_relation_data(
+    *, provider_endpoint: str, requirer_endpoint: str, include_default_juju_keys: bool = False
+):
+    """Get relation databags for a juju relation.
+
+    >>> get_relation_data('prometheus/0:ingress', 'traefik/1:ingress-per-unit')
+    """
+    provider_data, requirer_data = await asyncio.gather(
+        get_content(provider_endpoint, requirer_endpoint, include_default_juju_keys),
+        get_content(requirer_endpoint, provider_endpoint, include_default_juju_keys),
+    )
+    return RelationData(provider_data, requirer_data)
---
+++ tests/integration/test_charm_ipu.py
@@ -0,0 +1,60 @@
+# Copyright 2022 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+from pathlib import Path
+
+import pytest
+import yaml
+from pytest_operator.plugin import OpsTest
+
+from tests.integration.conftest import get_relation_data
+
+meta = yaml.safe_load((Path() / "metadata.yaml").read_text())
+resources = {name: val["upstream-source"] for name, val in meta["resources"].items()}
+
+
+@pytest.fixture(autouse=True)
+async def deployment(ops_test: OpsTest, traefik_charm):
+    if not ops_test.model.applications.get("traefik-k8s"):
+        await ops_test.model.deploy(traefik_charm, resources=resources)
+    await ops_test.model.applications["traefik-k8s"].set_config({"external_hostname": "foo.bar"})
+    await ops_test.juju("deploy", "prometheus-k8s", "--channel=edge")
+
+
+async def test_relate(ops_test: OpsTest):
+    await ops_test.juju("relate", "prometheus-k8s:ingress", "traefik-k8s:ingress-per-unit")
+    async with ops_test.fast_forward():
+        await ops_test.model.wait_for_idle(["traefik-k8s", "prometheus-k8s"])
+
+
+# @retry(wait=wait_exponential(multiplier=1, min=0, max=10))
+async def test_relation_data_shape(ops_test: OpsTest):
+    data = await get_relation_data(
+        requirer_endpoint="prometheus-k8s/0:ingress",
+        provider_endpoint="traefik-k8s/0:ingress-per-unit",
+    )
+
+    requirer_app_data = yaml.safe_load(data.requirer.application_data["data"])
+    # example:
+    # host: 10.1.232.176
+    # model: foo
+    # name: prometheus-k8s/0
+    # port: 9090
+    model = requirer_app_data["model"]
+    host = requirer_app_data["host"]
+    assert requirer_app_data == {
+        "host": host,
+        "model": model,
+        "name": "prometheus-k8s/0",
+        "port": "9090",
+    }
+
+    provider_app_data = yaml.safe_load(data.provider.application_data["data"])
+    # example:
+    #  ingress:
+    #   prometheus-k8s/0:
+    #     url: http://foo.bar:80/foo-prometheus-k8s-0
+
+    assert provider_app_data == {
+        "ingress": {"prometheus-k8s/0": {"url": f"http://foo.bar:80/{model}-prometheus-k8s-0"}}
+    }
