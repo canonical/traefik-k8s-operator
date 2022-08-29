a2872a46b4207a82207789d3c8fe77c40d71305a
sed-i <82407168+sed-i@users.noreply.github.com>
2022-08-29 17:29:26 -0400

    Make sure `ingress.url` returns an up-to-date value

--- lib/charms/traefik_k8s/v1/ingress.py
+++ lib/charms/traefik_k8s/v1/ingress.py
@@ -54,10 +54,10 @@ class SomeCharm(CharmBase):
 import logging
 import socket
 import typing
-from typing import Any, Dict, Optional, Tuple
+from typing import Any, Dict, Tuple
 
 import yaml
-from ops.charm import CharmBase, RelationEvent
+from ops.charm import CharmBase, RelationBrokenEvent, RelationEvent
 from ops.framework import EventSource, Object, ObjectEvents, StoredState
 from ops.model import ModelError, Relation
 
@@ -445,12 +445,17 @@ class IngressPerAppRequirer(_IngressPerAppBase):
 
         if self.is_ready():
             # Avoid spurious events, emit only when there is a NEW URL available
-            new_url = self.url
+            new_url = (
+                ""
+                if isinstance(event, RelationBrokenEvent)
+                else self._get_url_from_relation_data()
+            )
             if self._stored.current_url != new_url:
                 self._stored.current_url = new_url
                 self.on.ready.emit(event.relation, new_url)
 
     def _handle_relation_broken(self, event):
+        self._stored.current_url = ""
         self.on.revoked.emit(event.relation)
 
     def _handle_upgrade_or_leader(self, event):
@@ -461,7 +466,7 @@ class IngressPerAppRequirer(_IngressPerAppBase):
     def is_ready(self):
         """The Requirer is ready if the Provider has sent valid data."""
         try:
-            return bool(self.url)
+            return bool(self._get_url_from_relation_data())
         except DataValidationError as e:
             log.warning("Requirer not ready; validation error encountered: %s" % str(e))
             return False
@@ -504,15 +509,14 @@ class IngressPerAppRequirer(_IngressPerAppBase):
         """The established Relation instance, or None."""
         return self.relations[0] if self.relations else None
 
-    @property
-    def url(self) -> Optional[str]:
+    def _get_url_from_relation_data(self) -> str:
         """The full ingress URL to reach the current unit.
 
-        May return None if the URL isn't available yet.
+        Returns an empty string if the URL isn't available yet.
         """
         relation = self.relation
         if not relation:
-            return None
+            return ""
 
         # fetch the provider's app databag
         try:
@@ -522,11 +526,21 @@ class IngressPerAppRequirer(_IngressPerAppBase):
                 f"Error {e} attempting to read remote app data; "
                 f"probably we are in a relation_departed hook"
             )
-            return None
+            return ""
 
         if not raw:
-            return None
+            return ""
 
         ingress: ProviderIngressData = yaml.safe_load(raw)
         _validate_data({"ingress": ingress}, INGRESS_PROVIDES_APP_SCHEMA)
         return ingress["url"]
+
+    @property
+    def url(self) -> str:
+        """The full ingress URL to reach the current unit.
+
+        May return None if the URL isn't available yet.
+        """
+        data = self._stored.current_url or ""  # type: ignore
+        assert isinstance(data, str)  # for static checker
+        return data
