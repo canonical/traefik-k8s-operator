eb1b6288fa240f5e537269873de6d748cce94658
Pietro Pasotti <starfire.daemon@gmail.com>
2022-09-22 11:38:41 +0200

    added sequence test and playbook dump

--- tests/unit/test_lib_per_unit_provides_sequences.py
+++ tests/unit/test_lib_per_unit_provides_sequences.py
@@ -1,14 +1,15 @@
 # Copyright 2022 Canonical Ltd.
 # See LICENSE file for licensing details.
+from functools import partial
 
 import pytest
 import yaml
-from ops.charm import CharmBase
+from ops.charm import CharmBase, InstallEvent
 from ops.testing import Harness
 
 from charms.traefik_k8s.v1.ingress_per_unit import IngressPerUnitProvider
 from evt_sequences import Scenario, RelationMeta, RelationSpec, Model, Context, Emitter, Event, \
-    CharmSpec
+    CharmSpec, Scene, previous_context, Playbook
 
 
 class MockProviderCharm(CharmBase):
@@ -41,13 +42,14 @@ base_ctx = Context(model=Model(name='test-model'))
 related_ctx = base_ctx.replace(relations=[
     RelationSpec(meta=IPUMeta)
 ])
+ipu_data_base = {"port": '10',
+                 "host": 'foo.bar',
+                 "model": "test-model",
+                 "name": "remote/0",
+                 "mode": 'http'}
 base_ipu_relation_spec = RelationSpec(
     meta=IPUMeta,
-    units_data={0: {"port": '10',
-                    "host": 'foo.bar',
-                    "model": "test-model",
-                    "name": "remote/0",
-                    "mode": 'http'}})
+    units_data={0: ipu_data_base})
 
 # context in which the remote side (single unit) has
 # provided its side of the relation data.
@@ -71,7 +73,7 @@ def assert_ready(_, __, emitter: Emitter):
     assert h.charm.ipu.is_ready(relation)
 
 
-def assert_local_published_url(_, __, emitter: Emitter, url: str, value: bool):
+def assert_local_published_url(_, __, emitter: Emitter, url: str = None, value: bool = True):
     # check that the local side has published a url in their relation data.
     # and that that matches the proxied_endpoints
 
@@ -85,19 +87,27 @@ def assert_local_published_url(_, __, emitter: Emitter, url: str, value: bool):
         return
 
     for unit_dct in h.charm.ipu.proxied_endpoints.values():
-        assert unit_dct['url'] == url
+        if url:
+            assert unit_dct['url'] == url
+        else:
+            assert unit_dct['url']
+
     assert relation.data[h.model.app]['ingress'] == yaml.safe_dump({'remote/0': {'url': url}})
     assert not relation.data[h.model.unit], 'leader IPU providers should not have unit data'
 
 
-def assert_remote_data(_, __, emitter: Emitter):
+def assert_remote_data(_, __, emitter: Emitter, data: dict = None):
     # check that the remote unit has correct mocked data
     h: Harness = emitter.harness
+    data = data or {}
     relation = h.model.get_relation('ingress-per-unit')
     for unit in relation.units:
         unit_data = relation.data[unit]
         for key in ("port", "host", "model", "name", "mode"):
-            assert unit_data[key]
+            if data:
+                assert unit_data[key] == data.get(key)
+            else:
+                assert unit_data[key]
 
 
 def test_startup_sequence_leader():
@@ -160,3 +170,66 @@ def test_ingress_unit_provider_request_response(port, host, leader, url):
     # fail because unit isn't leader
     with pytest.raises(AssertionError):
         provider.publish_url(relation, unit_data["name"], "http://url/")
+
+
+def test_ingress_remains_consistent():
+    ipu_data_changed = {"port": '10',
+                        "host": 'choo.choo',  # changed
+                        "model": "test-model",
+                        "name": "remote/0",
+                        "mode": 'tcp'}
+    scenario = Scenario(charm_spec, Playbook(
+        (
+            Scene(Event('install'), base_ctx.replace(leader=False)),
+            # we gain leadership as we run config-changed
+            Scene(Event('config-changed'), previous_context.replace(leader=True)),
+            # previous context is assumed by default: we still have leadership
+            # as we run start
+            Scene(Event('start')),
+            Scene(Event('ingress-per-unit-relation-created'),
+                  previous_context.replace(relations=[RelationSpec(meta=IPUMeta)])),
+            Scene(Event('ingress-per-unit-relation-joined'),
+                  previous_context.replace(relations=[base_ipu_relation_spec])),
+            Scene(Event('ingress-per-unit-relation-changed'),
+                  previous_context.replace(relations=[base_ipu_relation_spec])),
+            Scene(Event('update-status')),
+            # now relation data REALLY changes!
+            Scene(Event('ingress-per-unit-relation-changed'),
+                  previous_context.replace(relations=[
+                      base_ipu_relation_spec.replace(
+                          units_data={0: ipu_data_changed})  # changed
+                  ])),
+            # add an unchanged teardown sequence at the boottom
+        )) + Scenario.builtins.TEARDOWN.playbook
+                        )
+
+    with scenario:
+        evt, _, _ = scenario.play_next()
+        assert isinstance(evt, InstallEvent)
+
+        evt, _, _ = scenario.play_next(3)  # skip to ipu-relation-created
+
+        # check that up until the teardown sequence,
+        # ipu has remote data and some local published url
+        scenario.play_next(
+            4,
+            assertions=[assert_ready,
+                        partial(assert_remote_data, data=ipu_data_base),
+                        assert_local_published_url]
+        )
+        # now the remote data ACTUALLY changes!
+        scenario.play_next(
+            4,
+            assertions=[assert_ready,
+                        partial(assert_remote_data, data=ipu_data_changed),
+                        assert_local_published_url]
+        )
+
+        # we simulate a teardown sequence: first step is break all relations,
+        # which means local and remote data will be gone, and IPU will be not ready.
+        scenario.embed(scenario.builtins.TEARDOWN,
+                assertions=[assert_not_ready,
+                            partial(assert_local_published_url, value=False)])
+
+    from pathlib import Path
+    (Path(__file__).parent / 'ipu.playbook').write_text(scenario.playbook.dump())
