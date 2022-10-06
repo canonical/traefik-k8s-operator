e748593be5bc3fd8bb4986876ddcdd15ed873e11
sed-i <82407168+sed-i@users.noreply.github.com>
2022-10-06 08:58:52 +0300

    Copy memoizer from Loki

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -1,7 +1,11 @@
 # Copyright 2022 Canonical Ltd.
 # See LICENSE file for licensing details.
+import functools
+import logging
 import os
+from collections import defaultdict
 from dataclasses import dataclass
+from datetime import datetime
 from pathlib import Path
 from subprocess import PIPE, Popen
 from time import sleep
@@ -17,9 +21,49 @@ trfk_resources = {name: val["upstream-source"] for name, val in trfk_meta["resou
 _JUJU_DATA_CACHE = {}
 _JUJU_KEYS = ("egress-subnets", "ingress-address", "private-address")
 
+logger = logging.getLogger(__name__)
+
+
+class Store(defaultdict):
+    def __init__(self):
+        super(Store, self).__init__(Store)
+
+    def __getattr__(self, key):
+        """Override __getattr__ so dot syntax works on keys."""
+        try:
+            return self[key]
+        except KeyError:
+            raise AttributeError(key)
+
+    def __setattr__(self, key, value):
+        """Override __setattr__ so dot syntax works on keys."""
+        self[key] = value
+
+
+store = Store()
+
+
+def timed_memoizer(func):
+    @functools.wraps(func)
+    async def wrapper(*args, **kwargs):
+        fname = func.__qualname__
+        logger.info("Started: %s" % fname)
+        start_time = datetime.now()
+        if fname in store.keys():
+            ret = store[fname]
+        else:
+            logger.info("Return for {} not cached".format(fname))
+            ret = await func(*args, **kwargs)
+            store[fname] = ret
+        logger.info("Finished: {} in: {} seconds".format(fname, datetime.now() - start_time))
+        return ret
+
+    return wrapper
+
 
 @pytest.fixture(autouse=True, scope="session")
 @pytest.mark.abort_on_fail
+@timed_memoizer
 def traefik_charm():
     proc = Popen(["charmcraft", "pack"], stdout=PIPE, stderr=PIPE, cwd=charm_root)
     proc.wait()
