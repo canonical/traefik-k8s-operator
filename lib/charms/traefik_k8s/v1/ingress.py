d878ea62188aee9b139bb483a20a618bb106e5ef
sed-i <82407168+sed-i@users.noreply.github.com>
2022-12-02 17:13:44 -0500

    Bump LIBPATCH

--- lib/charms/traefik_k8s/v1/ingress.py
+++ lib/charms/traefik_k8s/v1/ingress.py
@@ -69,7 +69,7 @@ LIBAPI = 1
 
 # Increment this PATCH version before using `charmcraft publish-lib` or reset
 # to 0 if you are raising the major API version
-LIBPATCH = 6
+LIBPATCH = 7
 
 DEFAULT_RELATION_NAME = "ingress"
 RELATION_INTERFACE = "ingress"
