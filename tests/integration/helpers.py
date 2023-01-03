d1f77a89b6f5e7b78cef7c6d5507b19ae25ec13f
sed-i <82407168+sed-i@users.noreply.github.com>
2023-01-03 15:25:15 -0500

    Add TLS itest

--- tests/integration/helpers.py
+++ tests/integration/helpers.py
@@ -3,6 +3,9 @@
 
 import asyncio
 import subprocess
+from typing import Optional
+
+from pytest_operator.plugin import OpsTest
 
 
 async def disable_metallb():
@@ -34,3 +37,21 @@ async def enable_metallb():
 
     await asyncio.sleep(30)  # why? just because, for now
     return ip
+
+
+async def get_address(ops_test: OpsTest, app_name: str, unit_num: Optional[int] = None) -> str:
+    """Find unit address for any application.
+
+    Args:
+        ops_test: pytest-operator plugin
+        app_name: string name of application
+        unit_num: integer number of a juju unit
+
+    Returns:
+        unit address as a string
+    """
+    status = await ops_test.model.get_status()
+    app = status["applications"][app_name]
+    return (
+        app["address"] if unit_num is None else app["units"][f"{app_name}/{unit_num}"]["address"]
+    )
--- tests/integration/test_traefik_deployed_after_metallb.py
+++ tests/integration/test_traefik_deployed_after_metallb.py
@@ -14,15 +14,19 @@ NOTE: This module implicitly relies on in-order execution (test running in the o
 """
 
 import asyncio
+import json
 import logging
+import tempfile
 from pathlib import Path
 from types import SimpleNamespace
+from urllib.parse import quote
+from urllib.request import urlopen
 
 import pytest
 import yaml
 from pytest_operator.plugin import OpsTest
 
-from tests.integration.helpers import disable_metallb, enable_metallb
+from tests.integration.helpers import disable_metallb, enable_metallb, get_address
 
 logger = logging.getLogger(__name__)
 
@@ -43,7 +47,7 @@ async def test_build_and_deploy(ops_test: OpsTest, traefik_charm):
     logger.info("First, disable metallb, in case it's enabled")
     await disable_metallb()
     logger.info("Now enable metallb")
-    ip = await enable_metallb()
+    await enable_metallb()
 
     await asyncio.gather(
         ops_test.model.deploy(
@@ -82,8 +86,12 @@ async def test_build_and_deploy(ops_test: OpsTest, traefik_charm):
 
     await ops_test.model.wait_for_idle(status="active", timeout=600, idle_period=30)
 
+
+@pytest.mark.abort_on_fail
+async def test_ingressed_endpoints_reachable_after_metallb_enabled(ops_test: OpsTest):
+    ip = get_address(ops_test, trfk.name)
     endpoints = [
-        f"{ip}/{path}"
+        quote(f"{ip}/{path}")
         for path in [
             f"{ops_test.model_name}-{ipr.name}",
             f"{ops_test.model_name}-{ipu.name}-0",
@@ -91,5 +99,62 @@ async def test_build_and_deploy(ops_test: OpsTest, traefik_charm):
         ]
     ]
     for ep in endpoints:
-        # FIXME make sure the response is from the workload and not from traefik
-        pass
+        urlopen(f"http://{ep}")
+        # A 404 would result in an exception:
+        #   urllib.error.HTTPError: HTTP Error 404: Not Found
+        # so just `urlopen` on its own should suffice for the test.
+
+
+@pytest.mark.abort_on_fail
+async def test_tls_termination(ops_test: OpsTest):
+    # TODO move this to the bundle tests
+    await ops_test.model.applications[trfk.name].set_config({"external_hostname": "juju.local"})
+
+    await ops_test.model.deploy(
+        "ch:tls-certificates-operator",
+        application_name="root-ca",
+        channel="edge",
+    )
+    await ops_test.model.applications["root-ca"].set_config(
+        {
+            "ca-common-name": "demo.ca.local",
+            "generate-self-signed-certificates": True,
+        }
+    )
+    await ops_test.model.add_relation("root-ca", f"{trfk.name}:certificates")
+    await ops_test.model.wait_for_idle(status="active", timeout=300)
+
+    # Get self-signed cert from peer app data
+    rc, stdout, stderr = await ops_test.run("juju", "show-unit", "root-ca/0", "--format=json")
+    data = json.loads(stdout)
+    peer_data = next(
+        filter(lambda d: d["endpoint"] == "replicas", data["root-ca/0"]["relation-info"])
+    )
+    cert = peer_data["application-data"]["self_signed_ca_certificate"]
+
+    with tempfile.TemporaryDirectory() as certs_dir:
+        cert_path = f"{certs_dir}/local.cert"
+        with open(cert_path, "wt") as f:
+            f.writelines(cert)
+
+        endpoints = [
+            quote(f"https://juju.local/{path}")
+            for path in [
+                f"{ops_test.model_name}-{ipr.name}",
+                f"{ops_test.model_name}-{ipu.name}-0",
+                f"{ops_test.model_name}-{ipa.name}",
+            ]
+        ]
+        ip = get_address(ops_test, trfk.name)
+        for endpoint in endpoints:
+            rc, stdout, stderr = await ops_test.run(
+                "curl",
+                "--resolve",
+                f"juju.local:443:{ip}",
+                "--capath",
+                certs_dir,
+                "--cacert",
+                cert_path,
+                endpoint,
+            )
+            logger.info("%s: %s", endpoint, (rc, stdout, stderr))
