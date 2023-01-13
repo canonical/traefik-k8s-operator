40ba5d7973338d27797cd1dbb836078dd238219f
Pietro Pasotti <starfire.daemon@gmail.com>
2023-01-13 14:35:57 +0100

    updated to new syntax

--- tests/interfaces/test_ingress.py
+++ tests/interfaces/test_ingress.py
@@ -1,31 +1,28 @@
 from unittest.mock import patch
 
-from interface_tester import InterfaceTester
+from pytest_interface_tester import InterfaceTester
+from scenario.structs import State
 
 from charm import TraefikIngressCharm
 
 
 def test_ingress_interface(interface_tester: InterfaceTester):
     with patch("charm.KubernetesServicePatch", lambda **unused: None):
+        interface_tester.configure(target=TraefikIngressCharm)
         interface_tester.run(
-            TraefikIngressCharm,
             interface_name='ingress',
-            config={"external_hostname": "0.0.0.0"},
+            state_template=State(
+                config={"external_hostname": "0.0.0.0"}
+            )
         )
 
 
-def test_ingress_per_unit_interface(interface_tester):
+def test_ingress_per_unit_interface(interface_tester: InterfaceTester):
     with patch("charm.KubernetesServicePatch", lambda **unused: None):
+        interface_tester.configure(target=TraefikIngressCharm)
         interface_tester.run(
-            TraefikIngressCharm,
             interface_name='ingress_per_unit',
-            config={"external_hostname": "0.0.0.0"},
-        )
-
-
-def test_all_interfaces(interface_tester):
-    with patch("charm.KubernetesServicePatch", lambda **unused: None):
-        interface_tester.run(
-            TraefikIngressCharm,
-            config={"external_hostname": "0.0.0.0"},
+            state_template=State(
+                config={"external_hostname": "0.0.0.0"}
+            )
         )
