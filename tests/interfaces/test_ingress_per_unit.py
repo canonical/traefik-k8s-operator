12b77f2fc8192f792fbe519a0e3f70c1d6402c28
Pietro Pasotti <starfire.daemon@gmail.com>
2023-01-23 17:00:42 +0100

    interface tests for ipa/ipu

--- tests/interfaces/test_ingress_per_unit.py
+++ tests/interfaces/test_ingress_per_unit.py
@@ -1,12 +1,12 @@
 from unittest.mock import patch
 
 from pytest_interface_tester import InterfaceTester
-from scenario.structs import State, NetworkSpec, network
+from scenario.structs import State
 
 from charm import TraefikIngressCharm
 
 
-def test_ingress_interface(interface_tester: InterfaceTester):
+def test_ingress_per_unit_interface(interface_tester: InterfaceTester):
     with patch("charm.KubernetesServicePatch", lambda **unused: None):
         interface_tester.configure(
             target=TraefikIngressCharm,
@@ -14,15 +14,8 @@ def test_ingress_interface(interface_tester: InterfaceTester):
             branch='tester'
         )
         interface_tester.run(
-            interface_name='ingress',
+            interface_name='ingress_per_unit',
             state_template=State(
-                config={"external_hostname": "0.0.0.0"},
-                networks=[
-                    NetworkSpec(
-                        'metrics-endpoint',
-                        bind_id=0,
-                        network=network()
-                    )
-                ]
+                config={"external_hostname": "0.0.0.0"}
             )
         )
