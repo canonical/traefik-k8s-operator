1db6c3484916e2778bd23e6749ab7f9e524ffa14
Pietro Pasotti <starfire.daemon@gmail.com>
2023-02-17 12:23:57 +0100

    undone

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -325,9 +325,9 @@ def assert_can_ping(ip, port):
 async def deploy_traefik_if_not_deployed(ops_test: OpsTest, traefik_charm):
     try:
         await ops_test.model.deploy(
-            traefik_charm, application_name="traefik-k8s",
-            resources=trfk_resources,
-            config={'external_hostname': "itesting.com"}
+            traefik_charm,
+            application_name="traefik-k8s",
+            resources=trfk_resources
         )
     except JujuError as e:
         if 'cannot add application "traefik-k8s": application already exists' not in str(e):
