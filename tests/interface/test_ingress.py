e566cf156f22e907db8ad4d1815ea24dc6ab4b91
Pietro Pasotti <starfire.daemon@gmail.com>
2023-04-18 09:29:48 +0200

    tox green

--- tests/interface/test_ingress.py
+++ tests/interface/test_ingress.py
@@ -3,16 +3,11 @@
 import pytest
 from interface_tester import InterfaceTester
 
-
 # Add here any charm interfaces that are registered to charm-relation-interfaces
-ALL_TESTABLE_INTERFACES = ['ingress', 'ingress_per_unit']
+ALL_TESTABLE_INTERFACES = ["ingress", "ingress_per_unit"]
 
 
-@pytest.mark.parametrize(
-    'interface_name',
-    ALL_TESTABLE_INTERFACES,
-    ids=ALL_TESTABLE_INTERFACES
-)
+@pytest.mark.parametrize("interface_name", ALL_TESTABLE_INTERFACES, ids=ALL_TESTABLE_INTERFACES)
 def test_ingress_interface(interface_tester: InterfaceTester, interface_name: str):
     interface_tester.configure(
         # TODO: remove when the tester branch hits main
