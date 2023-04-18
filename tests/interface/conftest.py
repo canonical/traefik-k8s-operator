cb73d48286ae32c70bd6e042ccdf5f4ddfc4316a
Pietro Pasotti <starfire.daemon@gmail.com>
2023-04-18 09:32:49 +0200

    comment for interface_tester

--- tests/interface/conftest.py
+++ tests/interface/conftest.py
@@ -10,6 +10,12 @@ from scenario.state import Container, State
 from charm import TraefikIngressCharm
 
 
+# Interface tests are centrally hosted at https://github.com/canonical/charm-relation-interfaces.
+# this fixture is used by the test runner of charm-relation-interfaces to test traefik's compliance
+# with the interface specifications.
+# DO NOT MOVE OR RENAME THIS FIXTURE! If you need to, you'll need to open a PR on
+# https://github.com/canonical/charm-relation-interfaces and change traefik's test configuration
+# to include the new identifier/location.
 @pytest.fixture
 def interface_tester(interface_tester: InterfaceTester):
     with patch("charm.KubernetesServicePatch", lambda **unused: None):
