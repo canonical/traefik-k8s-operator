092f3e3391dbebded049640e2c66dbb6f3859410
Pietro Pasotti <starfire.daemon@gmail.com>
2023-07-06 15:50:02 +0200

    lint

--- tests/scenario/test_middlewares.py
+++ tests/scenario/test_middlewares.py
@@ -1,15 +1,13 @@
 # Copyright 2023 Canonical Ltd.
 # See LICENSE file for licensing details.
 
-import itertools
 import tempfile
 import unittest
 from unittest.mock import MagicMock, PropertyMock, patch
 
 import pytest
 import yaml
-from charm import TraefikIngressCharm
-from scenario import Container, Context, Mount, Relation, State
+from scenario import Container, Mount, Relation, State
 
 
 def _render_middlewares(*, strip_prefix: bool = False, redirect_https: bool = False) -> dict:
@@ -32,7 +30,9 @@ def _render_middlewares(*, strip_prefix: bool = False, redirect_https: bool = Fa
     )
 
 
-def _render_config(*, rel_name:  str, routing_mode: str, strip_prefix: bool, redirect_https: bool, scheme: str):
+def _render_config(
+    *, rel_name: str, routing_mode: str, strip_prefix: bool, redirect_https: bool, scheme: str
+):
     routing_rule = {
         "path": "PathPrefix(`/test-model-remote-0`)",
         "subdomain": "Host(`test-model-remote-0.testhostname`)",
@@ -73,7 +73,7 @@ def _render_config(*, rel_name:  str, routing_mode: str, strip_prefix: bool, red
     }
 
     if middlewares := _render_middlewares(
-            strip_prefix=strip_prefix and routing_mode == "path", redirect_https=redirect_https
+        strip_prefix=strip_prefix and routing_mode == "path", redirect_https=redirect_https
     ):
         expected["http"].update(middlewares)
         expected["http"]["routers"]["juju-test-model-remote-0-router"].update(
@@ -87,7 +87,13 @@ def _render_config(*, rel_name:  str, routing_mode: str, strip_prefix: bool, red
 
 
 def _create_relation(
-        *, rel_id: int, rel_name: str, app_name: str, strip_prefix: bool, redirect_https: bool, scheme: str
+    *,
+    rel_id: int,
+    rel_name: str,
+    app_name: str,
+    strip_prefix: bool,
+    redirect_https: bool,
+    scheme: str,
 ):
     if rel_name == "ingress":
         app_data = {
@@ -108,7 +114,7 @@ def _create_relation(
             remote_app_name=app_name,
             relation_id=rel_id,
             remote_app_data=app_data,
-            remote_units_data={0: unit_data}
+            remote_units_data={0: unit_data},
         )
 
     if rel_name == "ingress-per-unit":
@@ -142,7 +148,9 @@ def _create_relation(
 @patch("charm.TraefikIngressCharm._traefik_service_running", PropertyMock(return_value=True))
 @patch("charm.TraefikIngressCharm._tcp_entrypoints_changed", MagicMock(return_value=False))
 @patch("charm.TraefikIngressCharm.version", PropertyMock(return_value="0.0.0"))
-def test_middleware_config(traefik_ctx, rel_name, routing_mode, strip_prefix, redirect_https, scheme):
+def test_middleware_config(
+    traefik_ctx, rel_name, routing_mode, strip_prefix, redirect_https, scheme
+):
     td = tempfile.TemporaryDirectory()
     containers = [
         Container(
@@ -161,7 +169,7 @@ def test_middleware_config(traefik_ctx, rel_name, routing_mode, strip_prefix, re
         app_name=app_name,
         strip_prefix=strip_prefix,
         redirect_https=redirect_https,
-        scheme=scheme
+        scheme=scheme,
     )
 
     # AND GIVEN external host is set (see also decorator)
@@ -177,7 +185,7 @@ def test_middleware_config(traefik_ctx, rel_name, routing_mode, strip_prefix, re
 
     # THEN the rendered config file contains middlewares
     with out.get_container("traefik").filesystem.open(
-            f"/opt/traefik/juju/juju_ingress_{rel_name}_{rel_id}_{app_name}.yaml",
+        f"/opt/traefik/juju/juju_ingress_{rel_name}_{rel_id}_{app_name}.yaml",
     ) as f:
         config_file = f.read()
     expected = _render_config(
@@ -185,10 +193,9 @@ def test_middleware_config(traefik_ctx, rel_name, routing_mode, strip_prefix, re
         routing_mode=routing_mode,
         strip_prefix=strip_prefix,
         redirect_https=redirect_https,
-        scheme=scheme
+        scheme=scheme,
     )
 
-    maxDiff = None
     assert expected == yaml.safe_load(config_file)
 
 
