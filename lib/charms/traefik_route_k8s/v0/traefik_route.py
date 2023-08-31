88434376a3c503896a5c3b137e73518fb21ad5bc
sed-i <82407168+sed-i@users.noreply.github.com>
2023-08-31 16:19:28 -0400

    Set scheme for traefik_route

--- lib/charms/traefik_route_k8s/v0/traefik_route.py
+++ lib/charms/traefik_route_k8s/v0/traefik_route.py
@@ -88,7 +88,7 @@ LIBAPI = 0
 
 # Increment this PATCH version before using `charmcraft publish-lib` or reset
 # to 0 if you are raising the major API version
-LIBPATCH = 7
+LIBPATCH = 8
 
 log = logging.getLogger(__name__)
 
@@ -141,7 +141,12 @@ class TraefikRouteProvider(Object):
     _stored = StoredState()
 
     def __init__(
-        self, charm: CharmBase, relation_name: str = "traefik-route", external_host: str = ""
+        self,
+        charm: CharmBase,
+        relation_name: str = "traefik-route",
+        external_host: str = "",
+        *,
+        scheme: str = "http",
     ):
         """Constructor for TraefikRouteProvider.
 
@@ -150,17 +155,17 @@ class TraefikRouteProvider(Object):
             relation_name: The name of the relation relation_name to bind to
                 (defaults to "traefik-route").
             external_host: The external host.
+            scheme: The scheme.
         """
         super().__init__(charm, relation_name)
-        self._stored.set_default(external_host=None)
+        self._stored.set_default(external_host=None, scheme=None)
 
         self._charm = charm
         self._relation_name = relation_name
 
-        if self._stored.external_host != external_host:
-            # If external_host changed, update
-            self._stored.external_host = external_host
-            self._update_requirers_with_external_host()
+        if self._stored.external_host != external_host or self._stored.scheme != scheme:
+            # If traefik endpoint details changed, update
+            self.update_traefik_address(external_host=external_host, scheme=scheme)
 
         self.framework.observe(
             self._charm.on[relation_name].relation_changed, self._on_relation_changed
@@ -172,15 +177,21 @@ class TraefikRouteProvider(Object):
     @property
     def external_host(self) -> str:
         """Return the external host set by Traefik, if any."""
-        self._update_stored_external_host()
+        self._update_stored()
         return self._stored.external_host or ""  # type: ignore
+
+    @property
+    def scheme(self) -> str:
+        """Return the scheme set by Traefik, if any."""
+        self._update_stored()
+        return self._stored.scheme or ""  # type: ignore
 
     @property
     def relations(self):
         """The list of Relation instances associated with this endpoint."""
         return list(self._charm.model.relations[self._relation_name])
 
-    def _update_stored_external_host(self) -> None:
+    def _update_stored(self) -> None:
         """Ensure that the stored host is up-to-date.
 
         This is split out into a separate method since, in the case of multi-unit deployments,
@@ -193,26 +204,37 @@ class TraefikRouteProvider(Object):
             for relation in self._charm.model.relations[self._relation_name]:
                 if not relation.app:
                     self._stored.external_host = ""
+                    self._stored.scheme = ""
                     return
                 external_host = relation.data[relation.app].get("external_host", "")
                 self._stored.external_host = external_host or self._stored.external_host  # type: ignore
+                scheme = relation.data[relation.app].get("scheme", "")
+                self._stored.scheme = scheme or self._stored.scheme  # type: ignore
 
     def _on_relation_changed(self, event: RelationEvent):
         if self.is_ready(event.relation):
             # todo check data is valid here?
-            self._update_requirers_with_external_host()
+            self.update_traefik_address()
             self.on.ready.emit(event.relation)
 
     def _on_relation_broken(self, event: RelationEvent):
         self.on.data_removed.emit(event.relation)
 
-    def _update_requirers_with_external_host(self):
+    def update_traefik_address(
+        self, *, external_host: Optional[str] = None, scheme: Optional[str] = None
+    ):
         """Ensure that requirers know the external host for Traefik."""
         if not self._charm.unit.is_leader():
             return
 
         for relation in self._charm.model.relations[self._relation_name]:
-            relation.data[self._charm.app]["external_host"] = self.external_host
+            relation.data[self._charm.app]["external_host"] = external_host or self.external_host
+            relation.data[self._charm.app]["scheme"] = scheme or self.scheme
+
+        # We first attempt to write relation data (which may raise) and only then update stored
+        # state.
+        self._stored.external_host = external_host
+        self._stored.scheme = scheme
 
     @staticmethod
     def is_ready(relation: Relation) -> bool:
