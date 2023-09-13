c07f4060fcdc5c4e165ddca714689ae8c13f1000
Pietro Pasotti <starfire.daemon@gmail.com>
2023-09-13 12:12:58 +0200

    minor refactoring of ingress v2 lib

--- tests/unit/test_ingress_lib.py
+++ tests/unit/test_ingress_lib.py
@@ -1,5 +1,6 @@
 import json
 
+import pytest
 from charms.traefik_k8s.v2.ingress import (
     IngressProviderAppData,
     IngressRequirerAppData,
@@ -7,14 +8,19 @@ from charms.traefik_k8s.v2.ingress import (
 )
 
 
-def test_io_ingress_requirer_unit_data():
-    databag = {}
+@pytest.mark.parametrize("clear", (True, False))
+def test_io_ingress_requirer_unit_data(clear):
+    databag = {"kaboom": '"boom"'}
     unit_data = IngressRequirerUnitData(host="foo.com")
 
-    unit_data.dump(databag)
-    assert databag == {
+    unit_data.dump(databag, clear=clear)
+    expected = {
         "host": '"foo.com"',
     }
+    if not clear:
+        expected["kaboom"] = '"boom"'
+
+    assert databag == expected
 
     assert IngressRequirerUnitData.load(databag) == unit_data
 
