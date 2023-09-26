812e5ec77b8f89c23d24ca761ae7c0f48c40edb6
Pietro Pasotti <starfire.daemon@gmail.com>
2023-09-26 10:48:56 +0200

    test docstrings

--- tests/unit/test_ingress_lib.py
+++ tests/unit/test_ingress_lib.py
@@ -10,6 +10,11 @@ from charms.traefik_k8s.v2.ingress import (
 
 @pytest.mark.parametrize("clear", (True, False))
 def test_io_ingress_requirer_unit_data(clear):
+    """Verify the 'clear' functionality of classes inheriting from DatabagModel.
+
+    Verify that if clear=True, when .dump()'ing data, any existing data in the databag is wiped;
+    else, it is kept.
+    """
     databag = {"kaboom": '"boom"'}
     unit_data = IngressRequirerUnitData(host="foo.com")
 
@@ -26,6 +31,8 @@ def test_io_ingress_requirer_unit_data(clear):
 
 
 def test_io_ingress_requirer_app_data():
+    """Round trip test: A.dump().load() == A for A = IngressRequirerAppData."""
+
     databag = {}
     app_data = IngressRequirerAppData(port=10, name="foo", model="coslite", strip_prefix=True)
 
@@ -45,6 +52,7 @@ def test_io_ingress_requirer_app_data():
 
 
 def test_aliases():
+    """Verify the pydantic aliasing functionality: pass 'strip_prefix', dump 'strip-prefix'."""
     databag = {}
     app_data = IngressRequirerAppData(
         port=10,
@@ -71,6 +79,8 @@ def test_aliases():
 
 
 def test_io_provider_data():
+    """Round trip test: A.dump().load() == A for A = IngressProviderAppData."""
+
     databag = {}
     url = {"url": "https://foo.com"}
     app_data = IngressProviderAppData(ingress=url)
