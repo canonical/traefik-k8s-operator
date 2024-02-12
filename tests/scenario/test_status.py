4df5895841a764b44fc6865e2b9ee91d7a681a9e
sed-i <82407168+sed-i@users.noreply.github.com>
2024-02-12 15:17:10 -0500

    Add test: block if no extweburl in subdomain mode

--- tests/scenario/test_status.py
+++ tests/scenario/test_status.py
@@ -3,7 +3,7 @@
 
 from unittest.mock import MagicMock, PropertyMock, patch
 
-from ops import ActiveStatus, WaitingStatus
+from ops import ActiveStatus, BlockedStatus, WaitingStatus
 from scenario import Container, State
 
 
@@ -49,4 +49,27 @@ def test_start_traefik_active(traefik_ctx, *_):
     out = traefik_ctx.run("start", state)
 
     # THEN unit status is `active`
+    assert out.unit_status == ActiveStatus("")
+
+
+@patch("charm.TraefikIngressCharm._external_host", PropertyMock(return_value="1.2.3.4"))
+@patch("traefik.Traefik.is_ready", PropertyMock(return_value=True))
+@patch("charm.TraefikIngressCharm._tcp_entrypoints_changed", MagicMock(return_value=False))
+def test_block_if_no_ext_host_in_subdomain_routing(traefik_ctx, *_):
+    # GIVEN external host is a bare IP from metallb (see decorator)
+    # WHEN a `config-change` hook fires and routing mode is subdomain
+    state = State(
+        config={"routing_mode": "subdomain"},
+        containers=[Container(name="traefik", can_connect=True)],
+    )
+    out = traefik_ctx.run("config-changed", state)
+
+    # THEN unit status is `blocked`
+    assert isinstance(out.unit_status, BlockedStatus)
+
+    # AND WHEN the routing mode is reverted to path
+    out.config["routing_mode"] = "path"
+
+    # THEN unit status is `active`
+    out = traefik_ctx.run("config-changed", out)
     assert out.unit_status == ActiveStatus("")
