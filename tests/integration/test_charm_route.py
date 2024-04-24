39f13875f5c2b42de5271dd832fd1c3cc9c944ea
Pietro Pasotti <starfire.daemon@gmail.com>
2024-04-24 10:36:44 +0200

    minor itest adjustments

--- tests/integration/test_charm_route.py
+++ tests/integration/test_charm_route.py
@@ -15,31 +15,34 @@ from tests.integration.conftest import (
 )
 from tests.integration.helpers import get_address
 
-APP_NAME = "traefik-k8s"
+APP_NAME = "traefik"
+TESTER_APP_NAME = "route"
 
 
 @pytest.mark.abort_on_fail
+@pytest.mark.setup
 async def test_deployment(ops_test: OpsTest, traefik_charm, route_tester_charm):
     await asyncio.gather(
         ops_test.model.deploy(traefik_charm, application_name=APP_NAME, resources=trfk_resources),
-        ops_test.model.deploy(route_tester_charm, "tr-tester"),
+        ops_test.model.deploy(route_tester_charm, TESTER_APP_NAME),
     )
 
-    await ops_test.model.wait_for_idle([APP_NAME, "tr-tester"], status="active", timeout=1000)
+    await ops_test.model.wait_for_idle([APP_NAME, TESTER_APP_NAME], status="active", timeout=1000)
 
 
-@pytest.mark.abort_on_fail
+@pytest.mark.setup
 async def test_relate(ops_test: OpsTest):
-    await ops_test.model.add_relation("tr-tester:traefik-route", "traefik-k8s:traefik-route")
-    await ops_test.model.wait_for_idle([APP_NAME, "tr-tester"])
+    await ops_test.model.add_relation(
+        f"{TESTER_APP_NAME}:traefik-route", f"{APP_NAME}:traefik-route"
+    )
+    await ops_test.model.wait_for_idle([APP_NAME, TESTER_APP_NAME])
 
 
-@pytest.mark.abort_on_fail
 async def test_dynamic_config_created(ops_test: OpsTest):
     relation = [
         r
         for r in ops_test.model.relations
-        if r.matches("tr-tester:traefik-route", "traefik-k8s:traefik-route")
+        if r.matches(f"{TESTER_APP_NAME}:traefik-route", f"{APP_NAME}:traefik-route")
     ][0]
     relation_id = relation.entity_id
     cmd = (
@@ -54,7 +57,6 @@ async def test_dynamic_config_created(ops_test: OpsTest):
     assert contents_yaml["some"] == "config"
 
 
-@pytest.mark.abort_on_fail
 async def test_static_config_updated(ops_test: OpsTest):
     cmd = f"juju ssh -m {ops_test.model_name} --container traefik {APP_NAME}/0 cat /etc/traefik/traefik.yaml"
     proc = Popen(shlex.split(cmd), stdout=PIPE, text=True)
@@ -65,17 +67,18 @@ async def test_static_config_updated(ops_test: OpsTest):
     assert contents_yaml["entryPoints"]["testPort"] == {"address": ":4545"}
 
 
-@pytest.mark.abort_on_fail
 async def test_added_entrypoint_reachable(ops_test: OpsTest):
-    traefik_ip = await get_address(ops_test, "traefik-k8s")
+    traefik_ip = await get_address(ops_test, APP_NAME, unit_num=0)
 
     req = Request(f"http://{traefik_ip}:4545")
 
     with pytest.raises(urllib.error.HTTPError, match="404"):
-        urlopen(req)
+        urlopen(req, timeout=5)
 
 
-@pytest.mark.abort_on_fail
+@pytest.mark.teardown
 async def test_remove_relation(ops_test: OpsTest):
-    await ops_test.juju("remove-relation", "tr-tester:traefik-route", f"{APP_NAME}:traefik-route")
+    await ops_test.juju(
+        "remove-relation", f"{TESTER_APP_NAME}:traefik-route", f"{APP_NAME}:traefik-route"
+    )
     await ops_test.model.wait_for_idle([APP_NAME], status="active")
