6f2895fffbcb3cf1a92523c3034dbf88eef01081
PietroPasotti <pietro.pasotti@canonical.com>
2024-06-06 16:44:39 +0200

    better error for proxied endpoints failure (#368)
    * better error for proxied endpoints failure
    
    * now the actual app name
    
    * generalized to multiple relations and guarded against relation.app being None

--- src/charm.py
+++ src/charm.py
@@ -430,7 +430,15 @@ class TraefikIngressCharm(CharmBase):
             try:
                 result.update(provider.proxied_endpoints)
             except Exception as e:
-                msg = f"failed to fetch proxied endpoints from provider {provider} with error {e}."
+                remote_app_names = [
+                    # relation.app could be None
+                    (relation.app.name if relation.app else "<unknown-remote>")
+                    for relation in provider.relations
+                ]
+                msg = (
+                    f"failed to fetch proxied endpoints from (at least one of) the "
+                    f"remote apps {remote_app_names!r} with error {e}."
+                )
                 event.log(msg)
 
         event.set_results({"proxied-endpoints": json.dumps(result)})
