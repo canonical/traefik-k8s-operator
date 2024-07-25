03a6aa9a91672b8952920700c74da63ede4a1559
Leon <82407168+sed-i@users.noreply.github.com>
2024-07-25 11:37:44 -0400

    Fix typo

--- tests/integration/test_forward_auth.py
+++ tests/integration/test_forward_auth.py
@@ -70,7 +70,7 @@ async def test_deployment(ops_test: OpsTest, traefik_charm, forward_auth_tester_
 
     await ops_test.model.integrate(f"{TRAEFIK_CHARM}:experimental-forward-auth", OATHKEEPER_CHARM)
 
-    # The auth lib uses event deferral, so by extention it depends on the update-status hook.
+    # The auth lib uses event deferral, so by extension it depends on the update-status hook.
     # As a result, when we use our 60m interval from the autouse fixture, test occasionally fail.
     # Here we override the interval just for this test.
     await ops_test.model.set_config({"update-status-hook-interval": "5m"})
