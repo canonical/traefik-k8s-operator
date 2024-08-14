dea5d7c73c0ae73fe6a02729796cd6fa61305444
Pedro Avalos Jimenez <pedro.avalosjimenez@canonical.com>
2024-08-14 09:10:26 -0500

    Bump LIBPATCH on tracing and ingress

--- lib/charms/tempo_k8s/v2/tracing.py
+++ lib/charms/tempo_k8s/v2/tracing.py
@@ -107,7 +107,7 @@ LIBAPI = 2
 
 # Increment this PATCH version before using `charmcraft publish-lib` or reset
 # to 0 if you are raising the major API version
-LIBPATCH = 7
+LIBPATCH = 8
 
 PYDEPS = ["pydantic"]
 
--- lib/charms/traefik_k8s/v2/ingress.py
+++ lib/charms/traefik_k8s/v2/ingress.py
@@ -73,7 +73,7 @@ LIBAPI = 2
 
 # Increment this PATCH version before using `charmcraft publish-lib` or reset
 # to 0 if you are raising the major API version
-LIBPATCH = 13
+LIBPATCH = 14
 
 PYDEPS = ["pydantic"]
 
