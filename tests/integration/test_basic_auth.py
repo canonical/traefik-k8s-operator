1eca9177569fe07f87c1adc2e88c1a2d38c96eac
Pietro Pasotti <starfire.daemon@gmail.com>
2024-09-03 09:30:24 +0200

    fixed itest

--- tests/integration/test_basic_auth.py
+++ tests/integration/test_basic_auth.py
@@ -2,6 +2,7 @@
 # See LICENSE file for licensing details.
 import asyncio
 import subprocess
+import time
 import urllib.request
 from urllib.error import HTTPError
 
@@ -18,9 +19,13 @@ from tests.integration.conftest import (
 USERNAME = "admin"
 PASSWORD = "admin"
 
+# we don't expect a 200 because ipa-tester has no real server listening
+SUCCESS_EXIT_CODE = 502
+
 # user:hashed-password pair generated via https://www.transip.nl/htpasswd/
 TEST_AUTH_USER = r"admin:$2a$13$XOHdzKdVS4mPKT0LvOfXru4LqyLbwcEvFlssXGS3laC6d/i6cKrLS"
 APP_NAME = "traefik"
+IPA = "ipa-tester"
 
 
 @pytest.mark.abort_on_fail
@@ -28,31 +33,32 @@ APP_NAME = "traefik"
 async def test_deployment(ops_test: OpsTest, traefik_charm, ipa_tester_charm):
     await asyncio.gather(
         ops_test.model.deploy(traefik_charm, application_name=APP_NAME, resources=trfk_resources),
-        ops_test.model.deploy(ipa_tester_charm, "ipa-tester"),
+        ops_test.model.deploy(ipa_tester_charm, IPA),
     )
 
-    await ops_test.model.wait_for_idle([APP_NAME, "ipa-tester"], status="active", timeout=1000)
+    await ops_test.model.wait_for_idle([APP_NAME, IPA], status="active", timeout=1000)
 
 
 @pytest.mark.abort_on_fail
 @pytest.mark.skip_on_deployed
 async def test_relate(ops_test: OpsTest):
     await ops_test.model.add_relation("ipa-tester:ingress", f"{APP_NAME}:ingress")
-    await ops_test.model.wait_for_idle([APP_NAME, "ipa-tester"])
+    await ops_test.model.wait_for_idle([APP_NAME, IPA])
 
 
-def get_tester_url(ops_test: OpsTest):
+def get_tester_url(model):
     data = get_relation_data(
-        requirer_endpoint="ipa-tester/0:ingress",
+        requirer_endpoint=f"{IPA}/0:ingress",
         provider_endpoint=f"{APP_NAME}/0:ingress",
-        model=ops_test.model_full_name,
+        model=model,
     )
     provider_app_data = yaml.safe_load(data.provider.application_data["ingress"])
     return provider_app_data["url"]
 
 
 @retry(stop=stop_after_delay(60 * 1))  # 5 minutes
-def get_url(url: str, expected: int, auth: str = None):
+def assert_get_url_returns(url: str, expected: int, auth: str = None):
+    print(f"attempting to curl {url} (with auth? {'yes' if auth else 'no'})")
     if auth:
         passman = urllib.request.HTTPPasswordMgrWithDefaultRealm()
         passman.add_password(None, url, USERNAME, PASSWORD)
@@ -65,55 +71,63 @@ def get_url(url: str, expected: int, auth: str = None):
     except HTTPError as e:
         if e.code == expected:
             return True
-        raise
+
+        print(f"unexpected exit code {e.code}")
+        time.sleep(0.1)
+        raise AssertionError
+
     if expected == 200:
         return True
+
+    print("unexpected 200")
+    time.sleep(0.1)
     raise AssertionError
 
 
+@pytest.fixture
+def model(ops_test):
+    return ops_test.model_full_name
+
+
 def set_basic_auth(model: str, user: str):
+    print(f"setting basic auth to {user!r}")
     option = f"basic_auth_user={user}" if user else "basic_auth_user="
     subprocess.run(["juju", "config", "-m", model, APP_NAME, option])
 
 
-async def test_ipa_charm_ingress_noauth(ops_test: OpsTest):
+def test_ipa_charm_ingress_noauth(model):
     # GIVEN basic auth is disabled (initial condition)
-    model_name = ops_test.model_full_name
-    set_basic_auth(model_name, "")
-    tester_url = get_tester_url(ops_test)
+    set_basic_auth(model, "")
+    tester_url = get_tester_url(model)
 
     # WHEN we GET the tester url
     # THEN we get it fine
-    assert get_url(tester_url, expected=200)
+    assert_get_url_returns(tester_url, expected=SUCCESS_EXIT_CODE)
 
 
-@pytest.mark.abort_on_fail
-async def test_ipa_charm_ingress_auth(ops_test: OpsTest):
+def test_ipa_charm_ingress_auth(model):
     # GIVEN basic auth is disabled (previous test)
-    model_name = ops_test.model_full_name
-    tester_url = get_tester_url(ops_test)
+    tester_url = get_tester_url(model)
 
     # WHEN we enable basic auth
-    set_basic_auth(model_name, TEST_AUTH_USER)
+    set_basic_auth(model, TEST_AUTH_USER)
 
     # THEN we can't GET the tester url
     # might take a little bit to apply the new config
     # 401 unauthorized
-    assert get_url(tester_url, expected=401)
+    assert_get_url_returns(tester_url, expected=401)
 
     # UNLESS we use auth
-    assert get_url(tester_url, expected=401, auth=TEST_AUTH_USER)
+    assert_get_url_returns(tester_url, expected=SUCCESS_EXIT_CODE, auth=TEST_AUTH_USER)
 
 
-@pytest.mark.abort_on_fail
-async def test_ipa_charm_ingress_auth_disable(ops_test: OpsTest):
+def test_ipa_charm_ingress_auth_disable(model):
     # GIVEN auth is enabled (previous test)
-    model_name = ops_test.model_full_name
-    tester_url = get_tester_url(ops_test)
+    tester_url = get_tester_url(model)
 
     # WHEN we disable it again
-    set_basic_auth(model_name, "")
+    set_basic_auth(model, "")
 
     # THEN we eventually can GET the endpoint without auth
     # might take a little bit to apply the new config
-    assert get_url(tester_url, expected=200)
+    assert_get_url_returns(tester_url, expected=SUCCESS_EXIT_CODE)
