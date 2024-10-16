62b758185d426537a4fb962ae229750a62f8e83f
Michael Thamm <mike.thamm@canonical.com>
2024-10-16 14:23:54 -0400

    Chore: Add integrations in outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,17 @@
 output "app_name" {
   value = juju_application.traefik.name
 }
+
+output "ingress_endpoint" {
+  description = "Name of the endpoint used by Traefik to provide ingress for the remote applications requesting it."
+  value       = "ingress"
+}
+
+output "ingress_per_unit_endpoint" {
+  description = "Name of the endpoint used by Traefik to provide ingress for the remote units requesting it."
+  value       = "ingress-per-unit"
+}
+
+output "traefik_route_endpoint" {
+  value       = "traefik-route"
+}
