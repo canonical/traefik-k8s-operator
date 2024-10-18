eb0e3983ece3a8c7d7c3404603d956bd0a4befd4
Michael Thamm <mike.thamm@canonical.com>
2024-10-18 11:18:14 -0400

    Feat: fix version pin and integration outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,16 +2,21 @@ output "app_name" {
   value = juju_application.traefik.name
 }
 
-output "ingress_endpoint" {
-  description = "Name of the endpoint used by Traefik to provide ingress for the remote applications requesting it."
-  value       = "ingress"
+output "requires" {
+  value = {
+    certificates              = "certificates",
+    experimental_forward_auth = "experimental-forward-auth",
+    logging                   = "logging",
+    tracing                   = "tracing",
+  }
 }
 
-output "ingress_per_unit_endpoint" {
-  description = "Name of the endpoint used by Traefik to provide ingress for the remote units requesting it."
-  value       = "ingress-per-unit"
-}
-
-output "traefik_route_endpoint" {
-  value       = "traefik-route"
+output "provides" {
+  value = {
+    grafana_dashboard = "grafana-dashboard",
+    ingress           = "ingress",
+    ingress_per_unit  = "ingress-per-unit",
+    metrics_endpoint  = "metrics-endpoint",
+    traefik_route     = "traefik-route",
+  }
 }
