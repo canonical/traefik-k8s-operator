1d56793ad20922dab172f4f2d59b96aed83fb579
Michael Thamm <mike.thamm@canonical.com>
2024-11-12 17:13:16 -0500

    Update the outputs.tf with endpoints nomenclature

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,21 +2,16 @@ output "app_name" {
   value = juju_application.traefik.name
 }
 
-output "requires" {
+output "endpoints" {
   value = {
     certificates              = "certificates",
     experimental_forward_auth = "experimental-forward-auth",
+    grafana_dashboard         = "grafana-dashboard",
+    ingress                   = "ingress",
+    ingress_per_unit          = "ingress-per-unit",
     logging                   = "logging",
+    metrics_endpoint          = "metrics-endpoint",
     tracing                   = "tracing",
-  }
-}
-
-output "provides" {
-  value = {
-    grafana_dashboard = "grafana-dashboard",
-    ingress           = "ingress",
-    ingress_per_unit  = "ingress-per-unit",
-    metrics_endpoint  = "metrics-endpoint",
-    traefik_route     = "traefik-route",
+    traefik_route             = "traefik-route",
   }
 }
