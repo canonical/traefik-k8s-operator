4e7e29bb2f5db426ead9e5380b9679b9cc362924
Dylan Stephano-Shachter <dylan.stephano-shachter@canonical.com>
2025-04-08 21:51:06 +0000

    bump libpatch

--- lib/charms/traefik_k8s/v2/ingress.py
+++ lib/charms/traefik_k8s/v2/ingress.py
@@ -84,7 +84,7 @@ LIBAPI = 2
 
 # Increment this PATCH version before using `charmcraft publish-lib` or reset
 # to 0 if you are raising the major API version
-LIBPATCH = 14
+LIBPATCH = 15
 
 PYDEPS = ["pydantic"]
 
