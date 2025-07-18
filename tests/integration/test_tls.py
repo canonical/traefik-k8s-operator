cf0f269d154b53c70edd265c37d771953308031b
Andrew Scribner <ca.scribner+1@gmail.com>
2025-07-18 10:38:51 -0400

    ci: temporarily disable known failing integration test

--- tests/integration/test_tls.py
+++ tests/integration/test_tls.py
@@ -164,7 +164,11 @@ async def pull_server_cert(ops_test, path):
         #        servers: []  # should not be empty!
 
 
-@pytest.mark.abort_on_fail
+@pytest.mark.xfail(
+        reason="Failing due to #491.  Remove this after #509 is implemented."
+               "See also: https://github.com/canonical/traefik-k8s-operator/issues/521"
+)
+# @pytest.mark.abort_on_fail
 async def test_tls_termination(ops_test: OpsTest, temp_dir):
     # TODO move this to the bundle tests
     await ops_test.model.applications[trfk.name].set_config({"external_hostname": mock_hostname})
