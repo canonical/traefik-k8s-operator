2a365a15e5c9910ccc700ed44ac761cdbfd72217
Dylan Stephano-Shachter <dylan.stephano-shachter@canonical.com>
2025-07-20 14:26:12 +0300

    Rebase reverse chain fix (#509)
    * update cert handler to fix https://github.com/canonical/traefik-k8s-operator/issues/491
    
    * Make the fix for https://github.com/canonical/traefik-k8s-operator/issues/491 backwards compatible
    
    * do not delete service as it causes unpredictable test issues

--- tests/integration/test_charm_route.py
+++ tests/integration/test_charm_route.py
@@ -14,7 +14,6 @@ from tests.integration.conftest import (
     trfk_resources,
 )
 from tests.integration.helpers import (
-    delete_k8s_service,
     get_k8s_service_address,
     remove_application,
 )
@@ -91,5 +90,4 @@ async def test_remove_relation(ops_test: OpsTest):
 
 
 async def test_cleanup(ops_test):
-    await delete_k8s_service(ops_test, f"{APP_NAME}-lb")
     await remove_application(ops_test, APP_NAME, timeout=60)
