5a7f117c27b09847b1fc1efcf5d7825e04fb27d1
Noctua <webops+observability-noctua-bot@canonical.com>
2025-09-16 22:03:07 +0200

    chore: update charm libraries (#543)

--- lib/charms/loki_k8s/v1/loki_push_api.py
+++ lib/charms/loki_k8s/v1/loki_push_api.py
@@ -544,7 +544,7 @@ LIBAPI = 1
 
 # Increment this PATCH version before using `charmcraft publish-lib` or reset
 # to 0 if you are raising the major API version
-LIBPATCH = 20
+LIBPATCH = 21
 
 PYDEPS = ["cosl"]
 
@@ -1621,15 +1621,21 @@ class ConsumerBase(Object):
                 deserialized_endpoint = json.loads(endpoint)
                 url = deserialized_endpoint.get("url")
 
-                # It's necessary to deduplicte the endpoints because if we don't do this, in the event that Loki coordinator is related to Flog and is scaled,
-                # in the /etc/promtail/promtail_config.yaml of Flog, there will duplicate entries for each unit of the Loki coordinator, which will cause Flog to go into error due to the duplication.
-                # The deduplication applied here
+                # Deduplicate by URL.
+                # With loki-k8s we have ingress-per-unit, so in that case
+                # we do want to collect the URLs of all the units.
+                # With loki-coordinator-k8s, even when the coordinator
+                # is scaled, we want to advertise only one URL.
+                # Without deduplication, we'd end up with the same
+                # tls config section in the promtail config file, in which
+                # case promtail immediately exits with the following error:
+                # [promtail] level=error ts=<timestamp> msg="error creating promtail" error="failed to create client manager: duplicate client configs are not allowed, found duplicate for name: "
 
                 if not url or url in seen_urls:
                     continue
 
                 seen_urls.add(url)
-                endpoints.append({"url": url})
+                endpoints.append(deserialized_endpoint)
 
         return endpoints
 
