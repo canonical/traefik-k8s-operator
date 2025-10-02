125506b79d7b6460c9f276b3e34055ed5c4d9e30
Thanhphan1147 <trung.thanh.phan@canonical.com>
2025-10-02 17:20:51 +0200

    bump ingress v1 lib version

--- lib/charms/traefik_k8s/v1/ingress.py
+++ lib/charms/traefik_k8s/v1/ingress.py
@@ -35,7 +35,7 @@ LIBAPI = 1
 
 # Increment this PATCH version before using `charmcraft publish-lib` or reset
 # to 0 if you are raising the major API version
-LIBPATCH = 19
+LIBPATCH = 20
 
 DEFAULT_RELATION_NAME = "ingress"
 RELATION_INTERFACE = "ingress"
