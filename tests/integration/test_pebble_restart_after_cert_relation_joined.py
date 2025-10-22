a9b40a38a70d4917de8bb74279ef4b6483beeda7
Sina <sina.pahlavan@canonical.com>
2025-10-22 10:29:32 -0400

    chore: update pebble restart itest

--- tests/integration/test_pebble_restart_after_cert_relation_joined.py
+++ tests/integration/test_pebble_restart_after_cert_relation_joined.py
@@ -14,16 +14,25 @@ import pytest
 import requests
 import yaml
 from pytest_operator.plugin import OpsTest
-
-from tests.integration.helpers import (
-    get_k8s_service_address,
-)
+from tenacity import retry, stop_after_attempt, wait_fixed
+from helpers import fetch_with_retry
 
 logger = logging.getLogger(__name__)
-
+import time
 METADATA = yaml.safe_load(Path("./metadata.yaml").read_text())
 resources = {"traefik-image": METADATA["resources"]["traefik-image"]["upstream-source"]}
 mock_hostname = "juju.local"
+
+async def get_traefik_url(ops_test: OpsTest, traefik_app_name: str = "traefik"):
+    """Get the URL for the Traefik app, as provided by the show-external-endpoints action."""
+    external_endpoints_action = (
+        await ops_test.model.applications[traefik_app_name]
+        .units[0]
+        .run_action("show-external-endpoints")
+    )
+    external_endpoints_action_results = (await external_endpoints_action.wait()).results
+    external_endpoints = yaml.safe_load(external_endpoints_action_results["external-endpoints"])
+    return external_endpoints[traefik_app_name]["url"]
 
 @pytest.mark.abort_on_fail
 async def test_build_and_deploy(ops_test: OpsTest, traefik_charm):
@@ -37,7 +46,7 @@ async def test_build_and_deploy(ops_test: OpsTest, traefik_charm):
         ops_test.model.deploy(
             "ch:alertmanager-k8s",
             application_name="alertmanager",
-            channel="1/stable",
+            channel="2/edge",
             trust=True,
         ),
         ops_test.model.deploy(
@@ -62,17 +71,17 @@ async def test_build_and_deploy(ops_test: OpsTest, traefik_charm):
 
 @pytest.mark.abort_on_fail
 async def test_can_route_ingress_using_tls(ops_test: OpsTest):
+    # It's important to test the behaviour by integrating traefik and ssc at this stage, after traefik has already been active/idle, meaning pebble has started traefik.
+    # This helps ascertain that Traefik behaves as expected when it is related to SSC.
     await asyncio.gather(
         ops_test.model.add_relation("ssc:certificates", "traefik"),
     )
 
-    traefik_address = await get_k8s_service_address(ops_test, "traefik-lb")
+    traefik_address = await get_traefik_url(ops_test, "traefik")
+    
+    alertmanager_address = f"{traefik_address}/{ops_test.model.info.name}-alertmanager"
 
-    # Both HTTP and HTTPS should work
-    alertmanager_address = f"https://{traefik_address}/{ops_test.model.info.name}-alertmanager"
-    response = requests.get(alertmanager_address)
-    assert response.status_code == 200
+    # Ensure we are able to get a 200 when calling AM. The helper asserts the status code.
+    response = fetch_with_retry(alertmanager_address)
 
-    alertmanager_address = f"http://{traefik_address}/{ops_test.model.info.name}-alertmanager"
-    response = requests.get(alertmanager_address)
-    assert response.status_code == 200
+
