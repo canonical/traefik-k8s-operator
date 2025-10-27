6a1282ac089e72213889c6b7420f68506022e912
tphan025 <trung.thanh.phan@canonical.com>
2025-10-27 14:51:40 +0100

    update tf code for provider v1

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,15 +1,19 @@
+data "juju_model" "model" {
+  name = var.model
+}
+
 resource "juju_application" "traefik" {
   name               = var.app_name
   config             = var.config
   constraints        = var.constraints
-  model              = var.model
+  model_uuid         = data.juju_model.model.id
   storage_directives = var.storage_directives
   trust              = true
   units              = var.units
 
   charm {
     name     = "traefik-k8s"
-    channel  = var.channel
+    channel  = var.channeluuid
     revision = var.revision
   }
 }
