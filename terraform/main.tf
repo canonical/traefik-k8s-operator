b0a97629d600c5fb5b15245e1b6801587aa072a4
tphan025 <trung.thanh.phan@canonical.com>
2025-10-30 15:29:02 +0100

    pin juju provider major version and update input to use model_uuid

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,12 +1,8 @@
-data "juju_model" "model" {
-  name = var.model
-}
-
 resource "juju_application" "traefik" {
   name               = var.app_name
   config             = var.config
   constraints        = var.constraints
-  model_uuid         = data.juju_model.model.id
+  model_uuid         = var.model_uuid
   storage_directives = var.storage_directives
   trust              = true
   units              = var.units
