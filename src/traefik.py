0480422b3f12b5a0e831156eabf83283fb86af7a
Ali Ugur <ali.ugur@canonical.com>
2025-11-26 12:41:59 +0300

    Chore: Remove telemetry

--- src/traefik.py
+++ src/traefik.py
@@ -110,7 +110,6 @@ class Traefik:  # pylint: disable=too-many-instance-attributes,too-many-public-m
         topology: JujuTopology,
         basic_auth_user: Optional[str] = None,
         tracing_endpoint: Optional[str] = None,
-        anonymous_telemetry_enabled: bool = True,
     ):
         """Initialize traefik service class.
 
@@ -124,7 +123,6 @@ class Traefik:  # pylint: disable=too-many-instance-attributes,too-many-public-m
             topology: Juju topology.
             basic_auth_user: User for basic auth.
             tracing_endpoint: Tracing endpoint.
-            anonymous_telemetry_enabled: Whether to enable anonymous telemetry.
         """
         self._container = container
         self._tcp_entrypoints = tcp_entrypoints
@@ -298,7 +296,6 @@ class Traefik:  # pylint: disable=too-many-instance-attributes,too-many-public-m
         static_config = {
             "global": {
                 "checknewversion": False,
-                "sendanonymoususage": self._anonymous_telemetry_enabled,
             },
             "log": {
                 "level": "DEBUG",
