16d65275542ff4484a0549227d0924b53ab5d274
swetha1654 <swetha.swaminathan@canonical.com>
2026-02-26 12:09:57 +0530

    libpatch

--- lib/charms/traefik_k8s/v1/ingress_per_unit.py
+++ lib/charms/traefik_k8s/v1/ingress_per_unit.py
@@ -83,7 +83,7 @@ LIBAPI = 1
 
 # Increment this PATCH version before using `charmcraft publish-lib` or reset
 # to 0 if you are raising the major API version
-LIBPATCH = 22
+LIBPATCH = 23
 
 log = logging.getLogger(__name__)
 
