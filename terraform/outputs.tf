980661e021347172a45db7da6eedb57ebb284b83
Swetha Swaminathan <swetha.swaminathan@canonical.com>
2026-06-08 12:29:56 +0530

    Address review comments

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,30 @@
+# Legacy outputs
+
+output "app_name" {
+  value = juju_application.traefik.name
+}
+
+output "endpoints" {
+  value = {
+    # Requires
+    certificates              = "certificates",
+    charm_tracing             = "charm-tracing",
+    experimental_forward_auth = "experimental-forward-auth",
+    logging                   = "logging",
+    receive_ca_cert           = "receive-ca-cert",
+    workload_tracing          = "workload-tracing",
+    upstream-ingress          = "upstream-ingress"
+    # Provides
+    grafana_dashboard = "grafana-dashboard",
+    ingress           = "ingress",
+    ingress_per_unit  = "ingress-per-unit",
+    metrics_endpoint  = "metrics-endpoint",
+    traefik_route     = "traefik-route",
+  }
+}
+
+# CC008 compliant outputs
+
 output "application" {
   description = "The deployed traefik-k8s application."
   value       = juju_application.traefik
