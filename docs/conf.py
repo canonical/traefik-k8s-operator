1589a8264e4295add9e05ac1e29419eb32e1e8c0
erinecon <erin.conley@canonical.com>
2026-07-15 13:26:51 -0400

    chore(docs): add sitemap_filename configuration

--- docs/conf.py
+++ docs/conf.py
@@ -155,6 +155,8 @@ html_baseurl = f"https://canonical.com/juju/docs/traefik-k8s-charm/{version}/"
 
 sitemap_url_scheme = '{link}'
 
+sitemap_filename = "doc-sitemap.xml"
+
 # Include `lastmod` dates in the sitemap:
 
 sitemap_show_lastmod = True
